74b93f7ceaaf8a120909b9a9d07cb792cefeeefe
rootm0s <rickardmalmkvist@hotmail.com>
2018-03-11 03:47:50 +0100

    Update encoder.py

--- framework/encoder.py
+++ framework/encoder.py
@@ -6,18 +6,32 @@ from includes import *
 key = 'Very long and confidential key LULZ'
 
 def encode(data):
+	"""
+	encode the data with the key and return the
+	data to function
+	"""
 	obfuscated = []
-	for x in range(len(data)):
-		key_c = key[x % len(key)]
-		enc_c = chr((ord(data[x]) + ord(key_c)) % 256)
-		obfuscated.append(enc_c)
-	return base64.urlsafe_b64encode("".join(obfuscated))
+	try:
+		for x in range(len(data)):
+			key_c = key[x % len(key)]
+			enc_c = chr((ord(data[x]) + ord(key_c)) % 256)
+			obfuscated.append(enc_c)
+		return base64.urlsafe_b64encode("".join(obfuscated))
+	except Exception as e:
+		pass
 
 def decode(data):
+	"""
+	decode the data with the key and return the
+	data to function
+	"""
 	deobfuscated = []
-	data = base64.urlsafe_b64decode(data)
-	for x in range(len(data)):
-		key_c = key[x % len(key)]
-		dec_c = chr((256 + ord(data[x]) - ord(key_c)) % 256)
-		deobfuscated.append(dec_c)
-	return "".join(deobfuscated)
+	try:	
+		data = base64.urlsafe_b64decode(data)
+		for x in range(len(data)):
+			key_c = key[x % len(key)]
+			dec_c = chr((256 + ord(data[x]) - ord(key_c)) % 256)
+			deobfuscated.append(dec_c)
+		return "".join(deobfuscated)
+	except Exception as e:
+		pass	
